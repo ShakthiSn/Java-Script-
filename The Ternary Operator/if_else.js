4236bf2a12d1bdb90953a7793919c5f286d55913
ShakthiSn <shakthi.snow23@gmail.com>
2024-12-02 18:56:18 +0530

    New switch

--- The Ternary Operator/if_else.js
+++ The Ternary Operator/if_else.js
@@ -12,4 +12,4 @@ gs.info("1.currently openstring value is "+openString); //=> closed
 
 //Introducing a shortcut way the ternary operator):
 var openStringStatus=(valueOpen) ? 'open' : 'closed';
-gs.info("1.currently openstring value is "+openStringStatus); //=> open
+console.info("1.currently openstring value is "+openStringStatus); //=> open
