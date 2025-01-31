2112a373cea04797eeac5d6dc300833639763c83
ShakthiSn <shakthi.snow23@gmail.com>
2025-01-31 18:42:33 +0530

    new

---
+++ Array_Method/join.js
@@ -0,0 +1,4 @@
+var len=['apple','pineapple','banana','grapes','pappaya'];
+gs.print(len); //apple,pineapple,banana,grapes,pappaya
+var sm=len.join('*');
+gs.print(sm); //apple,pineapple,banana,grapes,pappaya
