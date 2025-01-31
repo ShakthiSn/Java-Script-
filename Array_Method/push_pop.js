68844a5008ed210a868ed479ad360c3e12a575d8
ShakthiSn <shakthi.snow23@gmail.com>
2025-01-31 18:52:30 +0530

    new

---
+++ Array_Method/push_pop.js
@@ -0,0 +1,6 @@
+var len=['apple','pineapple','banana','grapes','pappaya'];
+gs.print(len);      //apple,pineapple,banana,grapes,pappaya
+len.push("berry");
+gs.print(len);      //apple,pineapple,banana,grapes,pappaya,berry
+len.pop();
+gs.print(len);     //apple,pineapple,banana,grapes,pappaya
