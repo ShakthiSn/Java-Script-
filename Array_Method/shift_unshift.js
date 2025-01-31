bd41cbde03d6ff5936e523e5dcce71db04214b13
ShakthiSn <shakthi.snow23@gmail.com>
2025-01-31 19:03:40 +0530

    new

---
+++ Array_Method/shift_unshift.js
@@ -0,0 +1,6 @@
+var len=['apple','pineapple','banana','grapes','pappaya'];
+gs.print(len);      //apple,pineapple,banana,grapes,pappaya
+len.shift();
+gs.print(len);  //pineapple,banana,grapes,pappaya
+len.unshift("gowva");
+gs.print(len);      //gowva,pineapple,banana,grapes,pappaya
