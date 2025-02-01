99be8b57e0996e0f2004a3ccc7ac63703d520fa8
ShakthiSn <shakthi.snow23@gmail.com>
2025-02-01 11:37:00 +0530

    new

---
+++ String Methods/replace_replaceAll.js
@@ -0,0 +1,7 @@
+var nae = "shakthi saranya shakthi indhu shakthi viji";
+var ss=nae.replace("shakthi","vel");
+gs.print(ss); //vel saranya shakthi indhu shakthi viji
+
+var nae1 = "shakthi saranya shakthi indhu shakthi viji";
+var s=nae1.replaceAll("shakthi","vel");
+gs.print(s); //vel saranya vel indhu vel viji
