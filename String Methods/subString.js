a509ed4093eb041b5747c66e73b16d4cbdb3c2ee
ShakthiSn <shakthi.snow23@gmail.com>
2025-02-01 15:12:37 +0530

    new

---
+++ String Methods/subString.js
@@ -0,0 +1,17 @@
+var para = "Start using ServiceNow's powerful Now Platform to build applications that make work better for your organization.";
+var slice=para.slice(25,15);
+gs.print(slice);// no output
+var substr = para.substring(25,15);
+gs.print(substr); //viceNow's 
+
+var slice1=para.slice(-25,15);
+gs.print(slice1);// no output
+var substr1 = para.substring(-25,15);
+gs.print(substr1); //Start using Ser  (- values considered as zero)
+
+
+
+
+
+
+
