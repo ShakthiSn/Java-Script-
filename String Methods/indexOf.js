facb1703baed811711d30d4bb6cbe80096063304
ShakthiSn <shakthi.snow23@gmail.com>
2025-02-01 12:24:58 +0530

    new

---
+++ String Methods/indexOf.js
@@ -0,0 +1,7 @@
+var para = "Start using ServiceNow's powerful Now Platform to build applications that make work better for your organization.";
+var ss = para.indexOf('Apple');
+gs.print(ss); // -1
+
+var para1 = "Start using ServiceNow's powerful Now Apple Platform to build applications that make work better for your organization.";
+var ss1 = para1.indexOf('Apple');
+gs.print(ss1); //38
