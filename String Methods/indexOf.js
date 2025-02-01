a4228d42a1b7c7b2ba1ef31a47816e69d28c490e
ShakthiSn <shakthi.snow23@gmail.com>
2025-02-01 16:25:39 +0530

    update

--- String Methods/indexOf.js
+++ String Methods/indexOf.js
@@ -5,3 +5,19 @@ gs.print(ss); // -1
 var para1 = "Start using ServiceNow's powerful Now Apple Platform to build applications that make work better for your organization.";
 var ss1 = para1.indexOf('Apple');
 gs.print(ss1); //38
+
+
+
+//eg 2
+
+var name3 = "shakthi saranya shakthi indhu Viji  ";
+
+if (name3.indexOf("Apple") >-1) 
+{
+    gs.print("found");
+} 
+else
+{
+    gs.print("not found"); //*** Script: not found
+
+}
