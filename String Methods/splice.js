7e02b8de1ae43598d2fd4459c1fc03fb4f7a9772
ShakthiSn <shakthi.snow23@gmail.com>
2025-02-01 13:05:35 +0530

    new

---
+++ String Methods/splice.js
@@ -0,0 +1,3 @@
+var para = "Start using ServiceNow's powerful Now Platform to build applications that make work better for your organization.";
+var ss = para.slice(0,13);
+gs.print(ss); //Start using S
