8ac7e2a3f35fded90dc720af4aeb5025a1baa4ec
ShakthiSn <shakthi.snow23@gmail.com>
2025-02-01 12:36:22 +0530

    new

---
+++ String Methods/trim.js
@@ -0,0 +1,3 @@
+var para = "                     Start using ServiceNow's powerful Now Platform to build applications that make work better for your organization.                   ";
+var ss = para.trim();
+gs.print(ss); // script: Start using ServiceNow's powerful Now Platform to build applications that make work better for your organization.
