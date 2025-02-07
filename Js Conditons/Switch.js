c54ea5b11b598a4600b54f25ca0d36757879652c
ShakthiSn <shakthi.snow23@gmail.com>
2025-02-07 19:22:41 +0530

    update

---
+++ Js Conditons/Switch.js
@@ -0,0 +1,35 @@
+var day=8;
+switch(day)
+{
+	case 1:
+	gs.print(" Day "+ day+" is Sunday !");
+	break;
+
+		case 2:
+	gs.print(" Day "+ day+" is Monday !");
+	break;
+
+		case 3:
+	gs.print(" Day "+ day+" is Tuesday !");
+	break;
+
+		case 4:
+	gs.print(" Day "+ day+" is Wednesday !");
+	break;
+
+		case 5:
+	gs.print(" Day "+ day+" is Thursday !");
+	break;
+
+		case 6:
+	gs.print(" Day "+ day+" is Friday !");
+	break;
+
+		case 7:
+	gs.print(" Day "+ day+" is Saturday !");
+	break;
+
+	default:
+	gs.print(" Day "+ day+" Invalid Date");
+
+}
