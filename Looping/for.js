6aae7a78a0d337ed56fe46b37215c970b766e7a7
ShakthiSn <shakthi.snow23@gmail.com>
2025-02-08 11:33:52 +0530

    update

--- Looping/for.js
+++ Looping/for.js
@@ -2,3 +2,19 @@ var num = 10;
 for (var i = 1; i <= num; i++) {
     gs.print(i);
 }
+
+//eg 2
+
+var nae="Shakthi";
+for(var i=0;i<nae.length;i++)
+{
+	gs.print(nae[i]);
+
+}
+/*** Script: S
+*** Script: h
+*** Script: a
+*** Script: k
+*** Script: t
+*** Script: h
+*** Script: i */ 
