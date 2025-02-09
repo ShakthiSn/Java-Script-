d968da417e99425e9f1b94cc5d56652951045c59
ShakthiSn <shakthi.snow23@gmail.com>
2025-02-09 17:22:46 +0530

    update

---
+++ Js Functions/methods.js
@@ -0,0 +1,26 @@
+function addValues()
+{
+	gs.print("welcome !");
+	
+}
+function addValue(a,b)
+{
+	var add=a+b;
+	gs.print(add);
+
+}
+addValues();
+addValue(5,10);
+
+function fullName(fName,lName)
+{
+	var fnl=fName+lName;
+	return fnl;
+}
+gs.print(fullName("Shakthi","Vel"));
+
+function emails(eMail)
+{
+	if(eMail.i)
+	
+}
