7abec42ed43e0a8372904823ace01a59a4401595
ShakthiSn <shakthi.snow23@gmail.com>
2025-03-26 10:20:25 +0530

    Glide User Information and User Role

--- Glide User Info and his roles/glideUser.js
+++ Glide User Info and his roles/glideUser.js
@@ -11,13 +11,11 @@ var sys_id=g_user.userID;
 alert(sys_id);
 
 
-var roleCheck=g_user.hasRole('abcd');
+var roleCheck=g_user.hasRole('itil');
 alert(roleCheck);   //it will check given the role and additionally check admin role also
- 
 
-
-var roleExact=g_user.hasRoleExactly('abcd');
+var roleExact=g_user.hasRoleExactly('itil');
 alert(roleExact); //it will check onle given the role not admin also
 
-var hasanyRole=g_user.hasRoles();
+var hasanyRole=g_user.hasRoles('itil','incident_manager','rest_services');
 alert(hasanyRole); //it will check given the roles and additionally check admin role also
